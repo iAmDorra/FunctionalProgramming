fa0de99ef339ace8b4453bcfc231889c99d031dd
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2021-01-12 00:21:31 +0100

    implement printer

--- Csharp/BankKata.Application/Printer.cs
+++ Csharp/BankKata.Application/Printer.cs
@@ -4,13 +4,18 @@ namespace BankKata
 {
     internal class Printer : IPrinter
     {
+        private string line = string.Empty;
+
         public void AddLine(DateTime dateTime, double amount, double balance)
         {
+            var date = dateTime.ToString("d");
+            line += "\n" + $"|{date}|{amount}|{balance}|";
         }
 
         public string Print()
         {
-            return null;
+            string header = "|Date|Amount|Balance|";
+            return header + line;
         }
     }
 }
---
+++ Csharp/BankKata.Tests/PrinterTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFluent;
+
+namespace BankKata.Tests
+{
+    [TestClass]
+    public class PrinterTest
+    {
+        [TestMethod]
+        public void Should_print_one_added_line()
+        {
+            var printer = new Printer();
+            System.DateTime date = new System.DateTime(2020, 01, 08);
+            double balance = 200;
+            double amount = 200;
+            printer.AddLine(date, amount, balance);
+
+            var printedLine = printer.Print();
+
+            string expectedLine = "|Date|Amount|Balance|" +
+                "\n" + "|08/01/2020|200|200|";
+            Check.That(printedLine).IsEqualTo(expectedLine);
+        }
+
+        [TestMethod]
+        public void Should_print_two_added_lines()
+        {
+            var printer = new Printer();
+            System.DateTime date = new System.DateTime(2020, 01, 08);
+            double balance = 200;
+            double amount = 200;
+            printer.AddLine(date, amount, balance);
+
+            System.DateTime secondDate = new System.DateTime(2020, 01, 09);
+            double secondAmount = 100;
+            printer.AddLine(secondDate, secondAmount, balance + secondAmount);
+
+            var printedLine = printer.Print();
+
+            string expectedLine = "|Date|Amount|Balance|" +
+                "\n" + "|08/01/2020|200|200|" +
+                "\n" + "|09/01/2020|100|300|";
+            Check.That(printedLine).IsEqualTo(expectedLine);
+        }
+    }
+}
