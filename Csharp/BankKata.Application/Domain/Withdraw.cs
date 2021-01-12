2a091ee9446e4042ed47a245e9fcdeb6d6d9a95e
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2021-01-12 17:33:11 +0100

    implement Withdraw UpdateBalance and Print

---
+++ Csharp/BankKata.Application/Domain/Withdraw.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BankKata
+{
+    internal class Withdraw : Transaction
+    {
+        private double amount;
+        private DateTime dateTime;
+
+        public Withdraw(double amount, DateTime dateTime)
+        {
+            this.amount = amount;
+            this.dateTime = dateTime;
+        }
+
+        public void Print(IPrinter printer, double balance)
+        {
+            printer.AddLine(dateTime, amount, balance);
+        }
+
+        public double UpdateBalance(double balance)
+        {
+            return balance - amount;
+        }
+    }
+}
---
+++ Csharp/BankKata.Tests/WithdrawTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFluent;
+using NSubstitute;
+using System;
+
+namespace BankKata
+{
+    [TestClass]
+    public class WithdrawTest
+    {
+        [TestMethod]
+        public void Should_update_balance_when_create_a_withdraw()
+        {
+            var balance = 2.1;
+            double amount = 1;
+            DateTime dateTime = new DateTime(2021, 01, 08);
+            Transaction withdraw = new Withdraw(amount, dateTime);
+            balance = withdraw.UpdateBalance(balance);
+            Check.That(balance).IsEqualTo(1.1);
+        }
+
+        [TestMethod]
+        public void Should_print_withdraw_with_right_info()
+        {
+            double balance = 2.1;
+            double amount = 1;
+            DateTime dateTime = new DateTime(2021, 01, 08);
+            Transaction withdraw = new Withdraw(amount, dateTime);
+
+            IPrinter printer = Substitute.For<IPrinter>();
+            withdraw.Print(printer, balance);
+
+            printer.Received(1).AddLine(dateTime, amount, balance);
+        }
+    }
+}
